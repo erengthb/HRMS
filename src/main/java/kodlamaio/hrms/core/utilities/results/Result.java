18136e09a9927bb1b6d64950b403fbed215df4dd
erengthb <erndmrhn@gmail.com>
2022-07-02 20:38:01 +0300

    core package added in project

---
+++ src/main/java/kodlamaio/hrms/core/utilities/results/Result.java
@@ -0,0 +1,21 @@
+package kodlamaio.hrms.core.utilities.results;
+
+public class Result {
+
+	private boolean succes;
+	
+	
+	private String message;
+	
+	public Result(boolean succes) {
+		
+		
+	}
+	
+	public Result (boolean succes, String message) {
+		
+		
+		
+	}
+	
+}
